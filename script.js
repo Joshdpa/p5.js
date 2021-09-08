d7c7c65eb41887e4f42e8660243138d4ac26018a
Joshua Allen <90309098+Joshdpa@users.noreply.github.com>
2021-09-08 08:06:14 +0000

    Witte en zwarte ballen

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-    background(255);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  noStroke()
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
